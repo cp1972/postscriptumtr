6ee5202bbc8ea92a0d2433cc26cba3ceb01b001e
Christian Papilloud <christian.papilloud@soziologie.uni-halle.de>
2024-07-02 11:26:17 +0200

    RmardownDocx.sh verändert

--- RmardownDOCX.sh
+++ RmardownDOCX.sh
@@ -2,10 +2,10 @@
 
 # convert Markdown/RMarkdown files to docx with bib and csl files in yaml preamble
 
-pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-ChapVDB.docx Chapter-VDB-ENG.rmd
+#pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-ChapVDB.docx Chapter-VDB-ENG.rmd
 #pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Vorwort.docx Vorwort.rmd
 #pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-1-Einführung.docx Einführung.rmd
-#pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-2-Gabe.docx Kap-2-Gabe.rmd
+pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-2-Gabe.docx Kap-2-Gabe.rmd
 #pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-3-Kunst.docx Kap-3-Kunst.rmd
 #pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-4-Stars.docx Kap-4-Stars.rmd
 #pandoc --to docx --from markdown --filter pandoc-citeproc -s -o CP-EMS-PS-Kap-5-Religion.docx Kap-5-Religion.rmd
